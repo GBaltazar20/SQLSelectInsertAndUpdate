8e837e69395ee979ef3a716aa29d5ab09446c6df
GBaltazar20 <g.baltazzar0022@gmail.com>
2025-11-22 14:33:40 +0800

    FrmClubRegistration created

--- SQLSelectInsertAndUpdate/Form1.Designer.cs
+++ SQLSelectInsertAndUpdate/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -212,6 +213,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(653, 378);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
